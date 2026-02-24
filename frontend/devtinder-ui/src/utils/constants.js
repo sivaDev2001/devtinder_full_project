5e05881be941cf6734ae24f4c6a0b4e2bdc60e8f
sivaDev2001 <siva2001avm@gmail.com>
2026-02-24 18:22:44 +0530

    localhost changed to render host

--- frontend/devtinder-ui/src/utils/constants.js
+++ frontend/devtinder-ui/src/utils/constants.js
@@ -1,2 +1,2 @@
-export const BASE_URL = import.meta.env.VITE_BASE_URL || "http://localhost:3000"
+export const BASE_URL = import.meta.env.VITE_BASE_URL || "https://devtinder-full-project.onrender.com"
 export const Profile_pic="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Cat_07464_kalamis_safinaz.jpg/250px-Cat_07464_kalamis_safinaz.jpg"
