26fd51e6f9eb67cf4b094c8115cd5dceda084173
sivaDev2001 <siva2001avm@gmail.com>
2026-03-17 12:55:31 +0530

    fixed nodem to node in package.json

--- frontend/devtinder-ui/src/utils/constants.js
+++ frontend/devtinder-ui/src/utils/constants.js
@@ -1,3 +1,3 @@
-export const BASE_URL = import.meta.env.VITE_BASE_URL || "https://devtinder-full-project.onrender.com"
+export const BASE_URL = import.meta.env.VITE_BASE_URL || "http://13.201.124.143:5000"
 
 //export const BASE_URL = import.meta.env.VITE_BASE_URL || "http://localhost:5000"
